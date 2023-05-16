e28cd2691086f1acd1bda10b157348b90a6b9335
Anupam Samanta <anupam96786@gmail.com>
2023-05-16 21:17:24 +0530

    read audio file implemented

--- emosic/emosic/views.py
+++ emosic/emosic/views.py
@@ -1,8 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import librosa
+import datetime
+import os
 
 
 @api_view(['POST'])
 def home(request):
-    print(request.FILES['audio'])
+    time = str(datetime.datetime.now())
+    webm_file = time + '.webm'
+    f = request.FILES['audio']
+    with open(webm_file, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    y, sr = librosa.load(webm_file, sr=22050)
+    print(y)
+    os.remove(webm_file)
     return Response({})
