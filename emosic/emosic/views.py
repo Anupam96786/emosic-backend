869a64e8028b536d6ce5bed55610c2fcfeb23e80
Anupam Samanta <anupam96786@gmail.com>
2023-05-17 01:02:19 +0530

    Feature Update
     - Improved model added
     - return emotion when api is called

--- emosic/emosic/views.py
+++ emosic/emosic/views.py
@@ -3,14 +3,17 @@ from rest_framework.response import Response
 import librosa
 import datetime
 import os
-import emosic_essentials
+from emosic_essentials import AudioAnalysis
 from keras.models import load_model
 from django.conf import settings
 import numpy as np
+import pickle
 
 
-TRAINED_MODEL = load_model(os.path.join(settings.BASE_DIR, 'model-0.8651.h5'))
+TRAINED_MODEL = load_model(os.path.join(settings.BASE_DIR, 'model-0.8723.h5'))
 print(TRAINED_MODEL.summary())
+with open(os.path.join(settings.BASE_DIR, 'feature_transcoder.pckl'), 'rb') as f:
+    a = pickle.load(f)
 
 
 @api_view(['POST'])
@@ -22,6 +25,5 @@ def home(request):
         for chunk in f.chunks():
             destination.write(chunk)
     y, sr = librosa.load(webm_file, sr=22050)
-    print(TRAINED_MODEL.predict(np.expand_dims(np.array(emosic_essentials.AudioAnalysis().get_features(y, sr)), 1).reshape(1, -1)))
     os.remove(webm_file)
-    return Response({})
+    return Response({'emotion': a.decode(TRAINED_MODEL.predict(np.expand_dims(np.array(AudioAnalysis().get_features(y, sr)), 1).reshape(1, -1))[0].tolist())})
