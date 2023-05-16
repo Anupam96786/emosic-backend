16cd0d2ab57fc3f0743f07798298f7df2c52a013
Anupam Samanta <anupam96786@gmail.com>
2023-05-16 23:55:45 +0530

    Predicting the output implemented

--- emosic/emosic/views.py
+++ emosic/emosic/views.py
@@ -4,6 +4,13 @@ import librosa
 import datetime
 import os
 import emosic_essentials
+from keras.models import load_model
+from django.conf import settings
+import numpy as np
+
+
+TRAINED_MODEL = load_model(os.path.join(settings.BASE_DIR, 'model-0.8651.h5'))
+print(TRAINED_MODEL.summary())
 
 
 @api_view(['POST'])
@@ -15,6 +22,6 @@ def home(request):
         for chunk in f.chunks():
             destination.write(chunk)
     y, sr = librosa.load(webm_file, sr=22050)
-    print(emosic_essentials.AudioAnalysis().get_features(y, sr))
+    print(TRAINED_MODEL.predict(np.expand_dims(np.array(emosic_essentials.AudioAnalysis().get_features(y, sr)), 1).reshape(1, -1)))
     os.remove(webm_file)
     return Response({})
